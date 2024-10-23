2e19d1fac2577d8dd3e2675c133bcc47388da9a9
Levi Planelles <leviplanellesgranata@gmail.com>
2024-10-23 16:21:21 +0200

    ej 1-6 tema4

--- src/main/java/com/leviplanelles/tema04/metodos/Ejercicio6.java
+++ src/main/java/com/leviplanelles/tema04/metodos/Ejercicio6.java
@@ -12,13 +12,15 @@ public class Ejercicio6 {
      * @param tamanio El número de veces que se imprimirá el caracter
      * @param lineas El numero de lineas que imprimirá el número de veces que se imprimirá el caracter
      */
-    public static void imprimirNum(char ch,int tamanio, int lineas) {
+    public static String imprimirNum(char ch,int tamanio, int lineas) {
+        String cadena = "";
         for (int j = 1; j <= lineas; j++) {
             for (int i = 1; i <= tamanio; i++) {
-                System.out.print(ch);
+                cadena += ch;
             }
-            System.out.println();
+            cadena += "\n";
         }
+        return cadena;
     }
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
@@ -27,7 +29,8 @@ public class Ejercicio6 {
         char caracter = leer.next().charAt(0);
         cantidad = IO.solicitarInt("Dime un número(0-10000): ",0,10000);
         lineas = IO.solicitarInt("Dime un número(0-10000): ",0,10000);
-        imprimirNum(caracter,cantidad,lineas);
+        String texto = imprimirNum(caracter,cantidad,lineas);
+        System.out.println(texto);
         leer.close();
     }
 }
