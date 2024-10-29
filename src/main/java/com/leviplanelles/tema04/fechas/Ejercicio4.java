1947d1132556bc68c1a116bde5cc509b9a36a0e5
Levi Planelles <leviplanellesgranata@gmail.com>
2024-10-29 23:24:56 +0100

    fechas ejercicios 4

--- src/main/java/com/leviplanelles/tema04/fechas/Ejercicio4.java
+++ src/main/java/com/leviplanelles/tema04/fechas/Ejercicio4.java
@@ -21,45 +21,48 @@ public class Ejercicio4 {
         SignoZodiacal signo;
         MonthDay mesDia = MonthDay.of(nacimiento.getMonth(), nacimiento.getDayOfMonth());
 
-        if (mesDia.isAfter(MonthDay.of(Month.FEBRUARY,18)) && mesDia.isBefore(MonthDay.of(Month.MARCH,11))) {
+        if (mesDia.isAfter(MonthDay.of(Month.FEBRUARY,17)) && mesDia.isBefore(MonthDay.of(Month.MARCH,12))) {
             signo = SignoZodiacal.ACUARIO;
-        } else if (mesDia.isAfter(MonthDay.of(Month.MARCH,12)) && mesDia.isBefore(MonthDay.of(Month.APRIL,16))) {
+        } else if (mesDia.isAfter(MonthDay.of(Month.MARCH,11)) && mesDia.isBefore(MonthDay.of(Month.APRIL,17))) {
             signo = SignoZodiacal.PISCIS;
-        } else if (mesDia.isAfter(MonthDay.of(Month.APRIL,17)) && mesDia.isBefore(MonthDay.of(Month.MAY,14))) {
+        } else if (mesDia.isAfter(MonthDay.of(Month.APRIL,16)) && mesDia.isBefore(MonthDay.of(Month.MAY,15))) {
             signo = SignoZodiacal.ARIES;
-        } else if (mesDia.isAfter(MonthDay.of(Month.MAY,15)) && mesDia.isBefore(MonthDay.of(Month.JUNE,21))) {
+        } else if (mesDia.isAfter(MonthDay.of(Month.MAY,14)) && mesDia.isBefore(MonthDay.of(Month.JUNE,22))) {
             signo = SignoZodiacal.TAURO;
-        } else if (mesDia.isAfter(MonthDay.of(Month.JUNE,22)) && mesDia.isBefore(MonthDay.of(Month.JULY,19))) {
+        } else if (mesDia.isAfter(MonthDay.of(Month.JUNE,21)) && mesDia.isBefore(MonthDay.of(Month.JULY,20))) {
             signo = SignoZodiacal.GEMINIS;
-        } else if (mesDia.isAfter(MonthDay.of(Month.JULY,20)) && mesDia.isBefore(MonthDay.of(Month.AUGUST,10))) {
+        } else if (mesDia.isAfter(MonthDay.of(Month.JULY,19)) && mesDia.isBefore(MonthDay.of(Month.AUGUST,11))) {
             signo = SignoZodiacal.CANCER;
-        } else if (mesDia.isAfter(MonthDay.of(Month.AUGUST,11)) && mesDia.isBefore(MonthDay.of(Month.SEPTEMBER,16))) {
+        } else if (mesDia.isAfter(MonthDay.of(Month.AUGUST,10)) && mesDia.isBefore(MonthDay.of(Month.SEPTEMBER,17))) {
             signo = SignoZodiacal.LEO;
-        } else if (mesDia.isAfter(MonthDay.of(Month.SEPTEMBER,17)) && mesDia.isBefore(MonthDay.of(Month.OCTOBER,30))) {
+        } else if (mesDia.isAfter(MonthDay.of(Month.SEPTEMBER,16)) && mesDia.isBefore(MonthDay.of(Month.OCTOBER,31))) {
             signo = SignoZodiacal.VIRGO;
-        } else if (mesDia.isAfter(MonthDay.of(Month.OCTOBER,31)) && mesDia.isBefore(MonthDay.of(Month.NOVEMBER,22))) {
+        } else if (mesDia.isAfter(MonthDay.of(Month.OCTOBER,30)) && mesDia.isBefore(MonthDay.of(Month.NOVEMBER,23))) {
             signo = SignoZodiacal.LIBRA;
-        } else if (mesDia.isAfter(MonthDay.of(Month.NOVEMBER,23)) && mesDia.isBefore(MonthDay.of(Month.NOVEMBER,28))) {
+        } else if (mesDia.isAfter(MonthDay.of(Month.NOVEMBER,22)) && mesDia.isBefore(MonthDay.of(Month.NOVEMBER,29))) {
             signo = SignoZodiacal.ESCORPIO;
-        } else if (mesDia.isAfter(MonthDay.of(Month.NOVEMBER,29)) && mesDia.isBefore(MonthDay.of(Month.DECEMBER,17))) {
+        } else if (mesDia.isAfter(MonthDay.of(Month.NOVEMBER,28)) && mesDia.isBefore(MonthDay.of(Month.DECEMBER,18))) {
             signo = SignoZodiacal.OFIUCO;
-        } else if (mesDia.isAfter(MonthDay.of(Month.DECEMBER,18)) && mesDia.isBefore(MonthDay.of(Month.JANUARY,20))) {
+        } else if (mesDia.isAfter(MonthDay.of(Month.DECEMBER,17)) && mesDia.isBefore(MonthDay.of(Month.JANUARY,21))) {
             signo = SignoZodiacal.SAGITARIO;
-        } else if (mesDia.isAfter(MonthDay.of(Month.JANUARY,21)) && mesDia.isBefore(MonthDay.of(Month.FEBRUARY,17))) {
+        } else if (mesDia.isAfter(MonthDay.of(Month.JANUARY,20)) && mesDia.isBefore(MonthDay.of(Month.FEBRUARY,18))) {
             signo = SignoZodiacal.CAPRICORNIO;
         } else {
             signo = SignoZodiacal.ERROR;
         }
         return signo;
     }
-
     public static void main(String[] args) {
 
         String fecha = IO.solicitarString("Ingresa una fecha(dd/mm/yyyy):",0,100);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate fechaNacimiento = LocalDate.parse(fecha,formatter);
         SignoZodiacal signo = signoZodiacal(fechaNacimiento);
-        System.out.println("Tu signo zodiacal es "+signo);
+        if (signo.equals(SignoZodiacal.ERROR)) {
+            System.out.println("No se ha encontrado tu signo zodiacal ¿?");
+        } else {
+            System.out.println("Tu signo zodiacal es "+signo);
+        }
 
     }
 }
